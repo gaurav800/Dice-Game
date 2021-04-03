97ba023475b88e5c3705b75396548c79da4580cd
Gaurav Yadav <47393893+gaurav800@users.noreply.github.com>
2021-04-04 04:21:49 +0530

    Updated the game with user prompts

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var playerOneName = prompt("Enter Player 1 Name: ");
+var playerTwoName = prompt("Enter Player 2 Name: ");
+
+document.querySelectorAll(".dice p")[0].innerText = playerOneName;
+document.querySelectorAll(".dice p")[1].innerText = playerTwoName;
+
+
 function myFunction() {
   var player1 = Math.ceil(Math.random() * 6);
   var player2 = Math.ceil(Math.random() * 6);
@@ -5,9 +12,9 @@ function myFunction() {
   document.querySelector(".img2").setAttribute("src", "images/dice" + player2 + ".png");
 
   if (player1 > player2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
+    document.querySelector("h1").innerHTML = "🚩 " + playerOneName + " wins";
   } else if (player2 > player1) {
-    document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
+    document.querySelector("h1").innerHTML = playerTwoName + " wins 🚩";
   } else {
     document.querySelector("h1").innerHTML = "Draw!";
   }
